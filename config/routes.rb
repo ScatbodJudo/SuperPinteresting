539b15a5799e4f074453438ee7cf6f336d9c6ee8
Todd Jacobus <rtjacobus@alaska.edu>
2016-02-16 11:30:47 -0700

    added a page as an outline of some ideas of the blog I would like to create some day...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" #creates about_path
+  get "outline" => "pages#outline" #creats outline_path???
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
