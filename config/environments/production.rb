0dc8efb92cbb97f91daafbf05316a58209a6e7e9
Todd Jacobus <rtjacobus@alaska.edu>
2016-02-24 08:55:57 -0700

    set up Amazon S3 service to allow image storage, installed AWS gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Rails.application.configure do
 #set to web host
 config.action_mailer.default_url_options = { host: 'https://superpinteresting.herokuapp.com/'}
 
+# sets Paperclip to save images to Amazon s3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
